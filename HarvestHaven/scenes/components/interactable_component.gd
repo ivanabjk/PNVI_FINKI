a5ca2895cab732d34b8a3fa45285ea1b33eb15c9
ivanabjk <ivana.bojkovska.002@gmail.com>
2025-07-22 17:46:30 +0200

    Created Guide NPC

--- HarvestHaven/scenes/components/interactable_component.gd
+++ HarvestHaven/scenes/components/interactable_component.gd
@@ -1,15 +1,13 @@
 class_name InteractableComponent
 extends Area2D
 
-signal intercatable_activated
+signal interactable_activated
 signal interactable_deactivated
 
 
+func _on_body_entered(_body: Node2D) -> void:
+	interactable_activated.emit()
 
 
-func _on_body_entered(body: Node2D) -> void:
-	intercatable_activated.emit()
-
-
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
 	interactable_deactivated.emit()
