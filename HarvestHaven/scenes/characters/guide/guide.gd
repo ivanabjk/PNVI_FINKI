5f04c35ee6b367709e58574d1db9d76a3156c60f
ivanabjk <ivana.bojkovska.002@gmail.com>
2025-07-23 00:57:50 +0200

    Implemented Dialogue

--- HarvestHaven/scenes/characters/guide/guide.gd
+++ HarvestHaven/scenes/characters/guide/guide.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-#var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
+var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
 
 @onready var interactable_component: InteractableComponent = $InteractableComponent
 @onready var interactable_label_component: Control = $InteractableLabelComponent
@@ -13,25 +13,25 @@ func _ready() -> void:
 	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
 	interactable_label_component.hide()
 	
-	#GameDialogueManager.give_crop_seeds.connect(on_give_crop_seeds)
+	GameDialogueManager.give_crop_seeds.connect(on_give_crop_seeds)
 
 
 func on_interactable_activated() -> void:
 	interactable_label_component.show()
-	#in_range = true
+	in_range = true
 
 
 func on_interactable_deactivated() -> void:
 	interactable_label_component.hide()
-	#in_range = false
+	in_range = false
 
 
-#func _unhandled_input(event: InputEvent) -> void:
-	#if in_range:
-		#if event.is_action_pressed("show_dialogue"):
-			#var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
-			#get_tree().root.add_child(balloon)
-			#balloon.start(load("res://dialogue/conversations/guide.dialogue"), "start")
+func _unhandled_input(event: InputEvent) -> void:
+	if in_range:
+		if event.is_action_pressed("show_dialogue"):
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+			get_tree().root.add_child(balloon)
+			balloon.start(load("res://dialogue/conversations/guide.dialogue"), "start")
 
 
 func on_give_crop_seeds() -> void:
